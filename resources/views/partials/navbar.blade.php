a774521ae57eb563b13bd1fd16c224c3d1fcde5b
deedthrone <deedthrone@protonmail.com>
2021-09-21 10:25:16 +0000

    Start Beginning

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,12 @@
+<div class="navbar">
+  <div class="logoNavbar">
+    <img src="image/logo/deedthrone.jpg" alt=""> 
+  </div>
+  <ul class="navlink">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Home</a></li>
+    <li><input type="search"></li>
+  </ul>
+</div>
