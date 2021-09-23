cee49dfa7dd65860f44705f3303758064bc4cc94
deedthrone <deedthrone@protonmail.com>
2021-09-23 16:55:38 +0000

    Progress Awal Login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,22 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="style/login.css">
     <title>Login</title>
 </head>
 <body>
     <div class="container">
-        <form action="index.html" method="post"></form>
         <h1>Login</h1>
-        <div class="username">
-            <label for=""> Username</label><input type="text" placeholder="Username">
-        </div>
-        <div class="password">
-            <label for=""> Password</label><input type="password" name="" id="" placeholder="Password">
-        </div>
-        <div class="submit">
-            <button>Submit</button>
-        </div>
+        <hr>
+        <form action="" method="post">
+            <div class="field">
+            <i class='bx bxs-user'></i>
+                <span class="tooltip"></span>
+            <input type="text" placeholder="Username" require>
+            <hr>
+            </div>
+            <div class="field">
+            <i class='bx bx-key' ></i>
+                <span class="tooltip"></span>
+            <input type="password" placeholder="Password" require>
+            <hr>
+            </div class="btn">
+                <input type="submit" placeholder="Submit" value="Login">
+            </div>
+        </form>
     </div>
 </body>
 </html>
