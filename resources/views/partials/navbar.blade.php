a26ec0028d36d2c55385f4fa7840696501e8e2c5
deedthrone <deedthrone@protonmail.com>
2021-10-05 16:52:29 +0000

    Modified UI

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,12 +1,27 @@
 <div class="navbar">
-    <ul>
-        <li><h1>Dashboard</h1></li>
-    </ul>
+    <h1>Dashboard</h1>
+        <ul>
 
-    <ul>
-        <li>
-        To Lead, Excel and be the best in all we do in order to achieve the ultimate client’s satisfaction.
-        </li>
-    </ul>
-</div>
-<hr>
+            <li>
+                <div class="search">
+                    <i class='bx bx-search-alt bx-sm'></i>
+                    <input type="text" placeholder="Search..">
+                </div>
+            </li>
+
+            <li>
+                <div class="notif">
+                    <button>
+                        <i class='bx bx-bell bx-sm'></i> 15
+                    </button>
+                </div>
+            </li>
+
+            <li>
+            <button>
+                Yosua Christopher 
+                <i class='bx bx-play bx-rotate-90'></i>
+                </button>
+            </li>
+        </ul>
+    </div>
