111537c138ee0adbd2b65b4c9a8afa57abbe8e41
deedthrone <deedthrone@protonmail.com>
2021-10-16 16:55:12 +0000

    Penambahan

--- resources/views/page/inbox.blade.php
+++ resources/views/page/inbox.blade.php
@@ -5,8 +5,21 @@
 @section('inbox')
 <!-- ISI -->
 
-<div class="chatfield">
-    asdas
+<div class="boxinbox">
+    <form action="" method="post">
+        <i class='bx bx-search-alt-2 bx-sm  ' ></i>
+        <input type="pencarian" name="" id="">
+    </form>
+</div>
+<div class="allbox">
+    <?php for ($i = 1 ; $i < 3 ; $i++ ) : ?>
+        <div class="submsg">
+            <button>
+                <i class='bx bxs-user-circle bx-md' ></i>
+                <label for=""><?= "nama"?></label>
+            </button>
+        </div>
+    <?php endfor; ?>
 </div>
 
 @endsection
